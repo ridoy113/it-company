eb660549483fc9872fd141e87303a17d0956937b
ridoy113 <almamounhridoy113@gmail.com>
2022-12-11 16:55:28 +0600

    navbar complite

--- src/components/shaered/navbar/Navbar.js
+++ src/components/shaered/navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import { GoThreeBars } from "react-icons/go";
+import navlogo from "../../../assets/Group 66.png";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
@@ -13,7 +14,7 @@ const Navbar = () => {
       <nav className="navbar">
         <div className="nav-container">
           <NavLink exact to="/" className="nav-logo">
-            CodeBucks
+            <img src={navlogo} alt="" />
             <i className="fas fa-code"></i>
           </NavLink>
 
@@ -64,7 +65,9 @@ const Navbar = () => {
             </li>
           </ul>
           <div className="nav-icon" onClick={handleClick}>
-            <i className={click ? <GoThreeBars /> : <GoThreeBars />}><GoThreeBars /></i>
+            <i className={click ? <GoThreeBars /> : <GoThreeBars />}>
+              <GoThreeBars />
+            </i>
           </div>
         </div>
       </nav>
