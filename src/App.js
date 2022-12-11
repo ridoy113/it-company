738782f21dcb799ab6c93ed80028ed695475e759
ridoy113 <almamounhridoy113@gmail.com>
2022-12-11 15:22:28 +0600

    add react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
 
 function App() {
   return (
-    <div>
-      <h1>ifjujhae9fu</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
