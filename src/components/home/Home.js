58adcbafa108198ba2d0970bc96dd1171387d9fb
ridoy113 <almamounhridoy113@gmail.com>
2022-12-12 16:20:38 +0600

    add home header responsive class

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,10 +6,12 @@ const Home = () => {
   return (
     <>
       <div className="home_header">
-        <div className="home_header_title">
-          <p>\ We Are Here \</p>
-          <h1>Better Insight For Business Growth</h1>
-          <button>View More</button>
+        <div className="home_header_first_title">
+          <div className="home_header_title">
+            <p>\ We Are Here \</p>
+            <h1>Better Insight For Business Growth</h1>
+            <button>View More</button>
+          </div>
         </div>
 
         <div className="home-header_img">
