9a0a32e0d78cff784867024221ea755d802a9ba7
ridoy113 <almamounhridoy113@gmail.com>
2022-12-12 15:36:27 +0600

    project update

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,8 +1,23 @@
 import React from "react";
 import "./home.css";
+import homeHeaderImg from "../../../src/assets/Group 18.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <div className="home_header">
+        <div className="home_header_title">
+          <p>\ We Are Here \</p>
+          <h1>Better Insight For Business Growth</h1>
+          <button>View More</button>
+        </div>
+
+        <div className="home-header_img">
+          <img src={homeHeaderImg} alt="" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
