0428ff8fe84adc5a27ab6dfd096f6de50b3fa5c5
ridoy113 <almamounhridoy113@gmail.com>
2022-12-13 17:47:25 +0600

    home planning json file found a error

---
+++ src/components/home/homePlanning/HomePlanning.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const HomePlanning = ({ homePlanning }) => {
+  const { id, img, numberImg, title, titleUnderImg, info } = homePlanning;
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <h1>{info}</h1>
+    </div>
+  );
+};
+
+export default HomePlanning;
--- src/components/home/homePlanning/HomePlannings.js
+++ src/components/home/homePlanning/HomePlannings.js
@@ -1,14 +1,31 @@
-import React from 'react';
-import './homePlannings.css';
-
-
+import React, { useEffect, useState } from "react";
+import HomePlanning from "./HomePlanning";
+import "./homePlannings.css";
 
 const HomePlannings = () => {
-    return (
-        <div>
-            Plannings
-        </div>
-    );
+  const [homePlannings, setHomePlannings] = useState([]);
+
+  useEffect(() => {
+    fetch("homePlanningsData.json")
+      .then((response) => response.json())
+      .then((data) => setHomePlannings(data));
+  }, []);
+
+  return (
+    <div>
+      <h4>\ Planning \</h4>
+      <h1>Our Process</h1>
+
+      <div>
+        {homePlannings.map((homePlanning) => (
+          <HomePlanning
+            key={homePlannings.id}
+            homePlanning={homePlanning}
+          ></HomePlanning>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default HomePlannings;
