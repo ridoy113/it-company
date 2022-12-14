36e8b10f4010039b23d23cfa246343b18b1852f5
ridoy113 <almamounhridoy113@gmail.com>
2022-12-14 22:18:22 +0600

    home plan styling start

--- src/components/home/homePlanning/HomePlannings.js
+++ src/components/home/homePlanning/HomePlannings.js
@@ -5,13 +5,15 @@ import homePlanDatas from "../../../data/homePlanData.json";
 const HomePlannings = () => {
   return (
     <div>
-      <h4>\ Planning \</h4>
-      <h1>Our Process</h1>
+      <div className="home_plan_header">
+        <h4>\ Planning \</h4>
+        <h1>Our Process</h1>
+      </div>
 
-      <div>
+      <div className="home_plan_grid_container">
         {homePlanDatas.map((homePlanData) => {
           return (
-            <div className="home_plan_grid_container">
+            <div className="home_plan_grid_single_card">
               <div className="home_plan_img_section">
                 <img
                   className="home_plan_img_1"
