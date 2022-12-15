c8f6803cf414ff374701a4e2c12f9516e8ca3d2b
ridoy113 <almamounhridoy113@gmail.com>
2022-12-15 22:09:07 +0600

    home services data create

--- src/components/home/homeServices/HomeServices.js
+++ src/components/home/homeServices/HomeServices.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./homeServices.css";
+import homeServicesData from "../../../data/homeServicesData.json";
 
 const HomeServices = () => {
   return <div>Home Services</div>;
