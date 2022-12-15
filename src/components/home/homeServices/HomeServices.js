2f68ab2231e5471f65177cc66f516135dac9413b
ridoy113 <almamounhridoy113@gmail.com>
2022-12-15 22:23:35 +0600

    json file data input in home services section

--- src/components/home/homeServices/HomeServices.js
+++ src/components/home/homeServices/HomeServices.js
@@ -1,9 +1,36 @@
 import React from "react";
 import "./homeServices.css";
-import homeServicesData from "../../../data/homeServicesData.json";
+import homeServicesDatas from "../../../data/homeServicesData.json";
 
 const HomeServices = () => {
-  return <div>Home Services</div>;
+  return (
+    <>
+      <div className="home_services_container">
+        <div className="home_services_title">
+          <h3>\ Services \</h3>
+          <h1>Our Expertice</h1>
+        </div>
+
+        <div className="home_services_grid_container">
+          {homeServicesDatas.map((homeServicesData) => {
+            return (
+              <div className="home_services_single_grid_card">
+                <div className="home_services_grid_img">
+                  <img src={homeServicesData.img} alt="" />
+                </div>
+                <h3>{homeServicesData.title}</h3>
+                <div className="home_services_grid_undertitleimg">
+                  <img src={homeServicesData.titleUnderImg} alt="" />
+                </div>
+
+                <p>{homeServicesData.info}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomeServices;
