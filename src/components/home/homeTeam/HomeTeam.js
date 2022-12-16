ec9098a9c06e315bf4c9caae436597275932e757
ridoy113 <almamounhridoy113@gmail.com>
2022-12-16 15:13:14 +0600

    home team all data input in section

--- src/components/home/homeTeam/HomeTeam.js
+++ src/components/home/homeTeam/HomeTeam.js
@@ -1,8 +1,37 @@
 import React from "react";
 import "./homeTeam.css";
+import homeTeamDatas from "../../../data/homeTeamData.json";
 
 const HomeTeam = () => {
-  return <div>Home Team</div>;
+  return (
+    <>
+      <div className="home_team_container">
+        <div className="home_team_title">
+          <div className="home_team_small_title">
+            <h3>\ Team \</h3>
+          </div>
+          <h1>Our Leaders</h1>
+        </div>
+
+        <div className="home_team_grid_container">
+          {homeTeamDatas.map((homeTeamData) => {
+            return (
+              <div className="home_team_single_grid_card">
+                <div className="home_team_grid-img">
+                  <img src={homeTeamData.img} alt="" />
+                </div>
+
+                <div className="home_team_grid_title">
+                  <h4>{homeTeamData.name}</h4>
+                  <p>{homeTeamData.jobTitle}</p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomeTeam;
