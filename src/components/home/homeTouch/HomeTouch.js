a3c1ef6690dcfbe98deff9e19b3dd2e95434b758
ridoy113 <almamounhridoy113@gmail.com>
2022-12-19 22:36:09 +0600

    home touch title data input

--- src/components/home/homeTouch/HomeTouch.js
+++ src/components/home/homeTouch/HomeTouch.js
@@ -2,7 +2,21 @@ import React from "react";
 import "./homeTouch.css";
 
 const HomeTouch = () => {
-  return <div>Home Touch</div>;
+  return (
+    <>
+      <div className="home_touch_section">
+        <div className="home_touch_container">
+          <div className="home_touch_title">
+            <h4>\ Get In Touch \</h4>
+            <h1>Hey! Let’s Talk</h1>
+          </div>
+
+
+          
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomeTouch;
