2f1747f81d6af9dc6efa2421afd64f583f125b12
ridoy113 <almamounhridoy113@gmail.com>
2022-12-29 22:48:09 +0600

    about first section complite

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HomeAbout from "../home/homeAbout/HomeAbout";
 import AboutHeader from "./aboutHeader/AboutHeader";
 
 const About = () => {
   return (
     <div>
       <AboutHeader />
+      <HomeAbout />
     </div>
   );
 };
