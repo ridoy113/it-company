6ef7522c7f4494a521b7eae14d5efa7a7bc49cd4
ridoy113 <almamounhridoy113@gmail.com>
2022-12-29 22:31:07 +0600

    about header title data added

--- src/components/about/aboutHeader/AboutHeader.js
+++ src/components/about/aboutHeader/AboutHeader.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./aboutHeader.css";
 
 const AboutHeader = () => {
-  return <div>Header</div>;
+  return (
+    <div className="about_header_container">
+      <div className="about_header_title">
+        <h1>About</h1>
+        <p>Home / About</p>
+      </div>
+    </div>
+  );
 };
 
 export default AboutHeader;
