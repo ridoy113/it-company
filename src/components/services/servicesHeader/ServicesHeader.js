ff74baba848c6a370fabdc3f4ba3cd6643aeee86
ridoy113 <almamounhridoy113@gmail.com>
2022-12-30 22:31:13 +0600

    our services header create

--- src/components/services/servicesHeader/ServicesHeader.js
+++ src/components/services/servicesHeader/ServicesHeader.js
@@ -2,7 +2,14 @@ import React from "react";
 import "./services.css";
 
 const ServicesHeader = () => {
-  return <div>Header</div>;
+  return (
+    <div className="about_header_container">
+      <div className="about_header_title">
+        <h1>Services</h1>
+        <p>Home / Services</p>
+      </div>
+    </div>
+  );
 };
 
 export default ServicesHeader;
