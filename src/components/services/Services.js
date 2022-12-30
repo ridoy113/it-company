3deca4c1d66fba117a0bb3e3da18ae3f3db1a8ec
ridoy113 <almamounhridoy113@gmail.com>
2022-12-30 22:34:32 +0600

    our services main section done

--- src/components/services/Services.js
+++ src/components/services/Services.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ServicesHeader from "./servicesHeader/ServicesHeader";
+import HomeWhat from "../home/homeWhat/HomeWhat";
+import HomeServices from "../home/homeServices/HomeServices";
 
 const Services = () => {
   return (
     <div>
       <ServicesHeader />
+      <HomeWhat />
+      <HomeServices />
     </div>
   );
 };
