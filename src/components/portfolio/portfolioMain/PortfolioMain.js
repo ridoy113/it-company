3aa717728b65e0cd8112aa9a9cb65d5b9a9eeb4a
ridoy113 <almamounhridoy113@gmail.com>
2023-01-02 10:25:15 +0600

    portfolio main section all data input

--- src/components/portfolio/portfolioMain/PortfolioMain.js
+++ src/components/portfolio/portfolioMain/PortfolioMain.js
@@ -1,17 +1,33 @@
 import React from "react";
 import "./portFolioMain.css";
+import gridImg1 from "../../../assets/Rectangle.png";
+import gridImg2 from "../../../assets/Rectangle (1).png";
+import gridImg3 from "../../../assets/Rectangle (2).png";
+import gridImg4 from "../../../assets/Rectangle (3).png";
+import gridImg5 from "../../../assets/Rectangle (4).png";
 
 const PortfolioMain = () => {
   return (
     <div className="portfolio_main_container">
       <div className="portfolio_main_grid-1">
-        <div>1</div>
-        <div>1</div>
+        <div className="portfolio_main_grid-1_img-1">
+          <img src={gridImg1} alt="" />
+        </div>
+        <div className="portfolio_main_grid-1_img-2">
+          <img src={gridImg2} alt="" />
+        </div>
       </div>
+
       <div className="portfolio_main_grid-2">
-        <div>1</div>
-        <div>1</div>
-        <div>1</div>
+        <div className="portfolio_main_grid-2_img-1">
+          <img src={gridImg3} alt="" />
+        </div>
+        <div className="portfolio_main_grid-2_img-2">
+          <img src={gridImg4} alt="" />
+        </div>
+        <div className="portfolio_main_grid-2_img-3">
+          <img src={gridImg5} alt="" />
+        </div>
       </div>
     </div>
   );
