fc3f8e71ab4f56b963004fc1262a53c4f8496f50
ridoy113 <almamounhridoy113@gmail.com>
2023-01-02 11:37:47 +0600

    portfolio section complite

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -1,12 +1,14 @@
 import React from "react";
 import PortfolioHeader from "./portfolioHeader/PortfolioHeader";
 import PortfolioMain from "./portfolioMain/PortfolioMain";
+import HomeLast from "../home/homeLast/HomeLast";
 
 const Portfolio = () => {
   return (
     <div>
       <PortfolioHeader />
       <PortfolioMain />
+      <HomeLast />
     </div>
   );
 };
