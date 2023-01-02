74709ed8fed4efbeffd85c73fd6701badb56778b
ridoy113 <almamounhridoy113@gmail.com>
2023-01-02 12:23:45 +0600

    contact page complite

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ContactHeader from "./contactHeader/ContactHeader";
 import ContactTouch from "./contactTouch/ContactTouch";
+import HomeLast from "../home/homeLast/HomeLast";
 
 const Contact = () => {
   return (
     <div>
       <ContactHeader />
       <ContactTouch />
+      <HomeLast />
     </div>
   );
 };
